5394f78b2291647dcfbbfce9fe2e91fe5e00d319
jameserv <106282058+jameserv@users.noreply.github.com>
2023-05-18 12:54:28 -0400

    Update chat app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from "react";
-import Chatbot from "react-chatbot-kit";
-import "./App.css";
-
-import ActionProvider from "./ActionProvider";
-import MessageParser from "./MessageParser";
-import config from "./config";
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const userMessage = {text: query, isUser: true};
+    const botMessage = {text: `You said: "${query}"`, isUser: false};
+    setMessages([...messages, userMessage, botMessage]);
+    setQuery('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Chatbot
-          config={config}
-          actionProvider={ActionProvider}
-          messageParser={MessageParser}
+      <h1>Chat Application</h1>
+      <div className="chatbox">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`message ${message.isUser ? 'user-message' : 'bot-message'}`}
+          >
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          placeholder="Type a message"
         />
-      </header>
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
 }
