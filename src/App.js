937b9e6f516d88ff78358d52935f31366186d010
aKssup <118090347+aKssup@users.noreply.github.com>
2023-05-18 14:25:10 -0400

    textbox + api integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
+
+//Setting up axios
+import axios from 'axios';
+
+//import textarea
+//import TextareaAutosize from 'react-textarea-autosize';
+
+
 import './App.css';
 
 function App() {
   const [query, setQuery] = useState('');
   const [messages, setMessages] = useState([]);
 
-  const handleSubmit = event => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const userMessage = {text: query, isUser: true};
-    const botMessage = {text: `You said: "${query}"`, isUser: false};
-    setMessages([...messages, userMessage, botMessage]);
+    const userMessage = { text: query, isUser: true };
+    const botMessagePlaceholder = { text: '...', isUser: false };
+
+    setMessages((prevMessages) => [...prevMessages, userMessage, botMessagePlaceholder]);
     setQuery('');
+
+    try {
+      //const response = await fetch(`http://localhost:1104/generateResponse?userText=${query}`);
+      const botText = await axios.get('http://localhost:1104/generateResponse', { params: { userText: query} });
+      const botMessage = { text: botText.data, isUser: false };
+      setMessages((prevMessages) => {
+        const updatedMessages = [...prevMessages];
+        const placeholderIndex = updatedMessages.findIndex((message) => message === botMessagePlaceholder);
+        updatedMessages.splice(placeholderIndex, 1, botMessage);
+        return updatedMessages;
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
@@ -26,15 +49,16 @@ function App() {
           </div>
         ))}
       </div>
+
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
+        <textarea
           value={query}
           onChange={e => setQuery(e.target.value)}
           placeholder="Type a message"
-        />
+        ></textarea>
         <input type="submit" value="Send" />
       </form>
+
     </div>
   );
 }
